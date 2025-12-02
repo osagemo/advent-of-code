b030a5a4520c83740499063569ee44ab73e95e0b
Oscar Sagemo <OscarS@bmsystem.se>
2025-12-02 11:37:57 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/osagemo/advent-of-code
+
+go 1.25.3
---
+++ utils/puzzle.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func PrepareInput(input string) string {
+	input = strings.ReplaceAll(input, "\r\n", "\n")
+	return strings.Trim(input, "\n")
+}
+
+func RunTimed(name string, fn func() int) {
+	start := time.Now()
+	fmt.Printf("%s: %d\n", name, fn())
+	fmt.Println(time.Since(start))
+}
