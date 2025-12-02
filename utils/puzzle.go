6bf5c4e0acf6d5fccd57fae1e1b7c38f6f292e13
Oscar Sagemo <OscarS@bmsystem.se>
2025-12-02 13:47:05 +0100

    add debug log

--- utils/puzzle.go
+++ utils/puzzle.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+var Debug = false
+
 func PrepareInput(input string) string {
 	input = strings.ReplaceAll(input, "\r\n", "\n")
 	return strings.Trim(input, "\n")
@@ -15,4 +17,10 @@ func RunTimed(name string, fn func() int) {
 	start := time.Now()
 	fmt.Printf("%s: %d\n", name, fn())
 	fmt.Println(time.Since(start))
+}
+
+func DebugLog(format string, a ...any) {
+	if Debug {
+		fmt.Printf(format, a...)
+	}
 }
